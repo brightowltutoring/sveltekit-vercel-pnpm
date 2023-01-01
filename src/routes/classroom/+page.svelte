493069dbb96a28a63913d07006cc5dd196179ef3
brightowltutoring <106844324+brightowltutoring@users.noreply.github.com>
2023-01-01 17:01:25 -0500

    in page.svelte of '/classroom' forgot to change the local storage dark mode getter name to 'isDarkModeLS'

--- src/routes/classroom/+page.svelte
+++ src/routes/classroom/+page.svelte
@@ -3,7 +3,7 @@
   import { onMount, onDestroy } from "svelte";
   import { isDarkMode } from "$lib/store";
 
-  const wasDarkModeLS = localStorage.getItem("darkMode");
+  const wasDarkModeLS = localStorage.getItem("isDarkModeLS");
   // this is to decide in onDestroy to reset to lightmode when navigating away from '/classroom', or to remain in darkmode ... provided darkmode was on before navigating to
 
   onMount(() => {
